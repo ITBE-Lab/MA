9447dacc631455dbaa408a99a717177b0cda3ed3
Markus <markus.rainer.schmidt@gmail.com>
2017-03-02 13:34:00 +0900

    will move everythin into one module last commit before that

--- inc/core/aligner.h
+++ inc/core/aligner.h
@@ -22,13 +22,13 @@ public:
     {}
 
     void setData(std::shared_ptr<Container> pC){pCurrent = pC;}
-    void addModule(Module xM){lModules.push_back(xM);std::cout << "added Module" << std::endl;}
+    void addModule(/*Module xM*/){/*lModules.push_back(xM);*/std::cout << "added Module" << std::endl;}
     void step(){if(lModules.empty())return; pCurrent = lModules.front().execute(pCurrent); lModules.pop_front();}
     void steps(){while(!lModules.empty())step();}
 
     void test()
     {
-        addModule(Printer());
+       // addModule(Printer());
         step();
     }
 
--- pythonTest.py
+++ pythonTest.py
@@ -1,9 +1,9 @@
 
 from aligner.core import aligner
-from aligner.modules import module
+#from aligner.modules import module
 
 a = aligner.Aligner()
 
-a.addModule(module.Printer())
+a.addModule()
 
 a.step()
