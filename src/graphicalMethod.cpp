4e03dfb1a3c98cc4de62505420d73f836051bdeb
Markus <markus.rainer.schmidt@gmail.com>
2017-03-06 21:20:22 +0900

    all containers need to be refractored they will have to become decorators using pointers internally in order to avoid memory management troubles with python

---
+++ src/graphicalMethod.cpp
@@ -0,0 +1,12 @@
+#incldue "graphicalMethod.h"
+
+
+void exportGraphicalMethod()
+{
+    /* TODO: activate ME
+	boost::python::class_<AnchorMatchList, boost::python::bases<Container>, std::shared_ptr<AnchorMatchList>>("AnchorMatchList");
+    
+	//tell boost python that it's possible to convert shared pointers with these classes
+    boost::python::implicitly_convertible<std::shared_ptr<AnchorMatchList>,std::shared_ptr<Container>>();
+    */
+}
