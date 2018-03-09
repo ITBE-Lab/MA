1a9d097fd8b9285840ebd565849cf0f72154a14c
Markus <markus.rainer.schmidt@gmail.com>
2018-03-09 18:03:02 +0900

    cleaning up in python code as well

--- MA/aligner.py
+++ MA/aligner.py
@@ -226,24 +226,6 @@ class BinarySeeding(libMA.BinarySeeding):
         return super(BinarySeeding, self).promise_me(ContainerVector(*args))
 
 ##
-# @brief python wrapper for Minimizers
-class Minimizers(libMA.Minimizers):
-    def execute(self, *args):
-        return super(Minimizers, self).execute(ContainerVector(*args))
-
-    def promise_me(self, *args):
-        return super(Minimizers, self).promise_me(ContainerVector(*args))
-
-##
-# @brief python wrapper for MinimizersToSeeds
-class MinimizersToSeeds(libMA.MinimizersToSeeds):
-    def execute(self, *args):
-        return super(MinimizersToSeeds, self).execute(ContainerVector(*args))
-
-    def promise_me(self, *args):
-        return super(MinimizersToSeeds, self).promise_me(ContainerVector(*args))
-
-##
 # @brief python wrapper for StripOfConsideration
 class StripOfConsideration(libMA.StripOfConsideration):
     def execute(self, *args):
@@ -251,15 +233,6 @@ class StripOfConsideration(libMA.StripOfConsideration):
 
     def promise_me(self, *args):
         return super(StripOfConsideration, self).promise_me(ContainerVector(*args))
-
-##
-# @brief python wrapper for StripOfConsideration2
-class StripOfConsideration2(libMA.StripOfConsideration2):
-    def execute(self, *args):
-        return super(StripOfConsideration2, self).execute(ContainerVector(*args))
-
-    def promise_me(self, *args):
-        return super(StripOfConsideration2, self).promise_me(ContainerVector(*args))
 
 ##
 # @brief python wrapper for LinearLineSweep
@@ -340,19 +313,6 @@ class ExtractAllSeeds(libMA.ExtractAllSeeds):
 
     def promise_me(self, *args):
         return super(ExtractAllSeeds, self).promise_me(ContainerVector(*args))
-
-##
-# @brief The ReSeed Module.
-# @details
-# extracts all seeds from a segment list.
-# @ingroup module
-#
-class ReSeed(libMA.ReSeed):
-    def execute(self, *args):
-        return super(ReSeed, self).execute(ContainerVector(*args))
-
-    def promise_me(self, *args):
-        return super(ReSeed, self).promise_me(ContainerVector(*args))
 
 ##
 # @brief The SMW Module.
