b305bd8d9d53731752b1c69758c32b5f621d9361
Markus <markus.rainer.schmidt@gmail.com>
2018-11-13 18:21:08 +0900

    added zlib

--- libs/ma/inc/module/fileReader.h
+++ libs/ma/inc/module/fileReader.h
@@ -10,12 +10,199 @@
 #include "module/module.h"
 
 
+#ifdef WITH_ZLIB
+#include "zlib.h"
+#endif
+
+
 /// @cond DOXYGEN_SHOW_SYSTEM_INCLUDES
 #include <fstream>
 /// @endcond
 
 namespace libMA
 {
+
+class FileStream
+{
+  public:
+    FileStream( )
+    {}
+
+    FileStream( const FileStream& ) = delete;
+
+    virtual bool is_open( ) const
+    {
+        throw AnnotatedException( "Unimplemented" );
+    }
+    virtual bool eof( ) const
+    {
+        throw AnnotatedException( "Unimplemented" );
+    }
+    virtual void close( )
+    {
+        throw AnnotatedException( "Unimplemented" );
+    }
+    virtual size_t tellg( )
+    {
+        throw AnnotatedException( "Unimplemented" );
+    }
+    virtual char peek( )
+    {
+        throw AnnotatedException( "Unimplemented" );
+    }
+    virtual void safeGetLine( std::string& t )
+    {
+        throw AnnotatedException( "Unimplemented" );
+    }
+}; // class
+
+class StdFileStream : public FileStream
+{
+  private:
+    std::ifstream xStream;
+
+  public:
+    StdFileStream( std::string sFilename ) : xStream( sFilename )
+    {} // constructor
+
+    bool is_open( ) const
+    {
+        return xStream.is_open( );
+    } // method
+
+    bool eof( ) const
+    {
+        return !xStream.good( ) || xStream.eof( );
+    } // method
+
+    void close( )
+    {
+        xStream.close( );
+    } // method
+
+    char peek( )
+    {
+        return xStream.peek( );
+    } // method
+
+    size_t tellg( )
+    {
+        return xStream.tellg( );
+    } // method
+
+    /**
+     * code taken from
+     * https://stackoverflow.com/questions/6089231/getting-std-ifstream-to-handle-lf-cr-and-crlf
+     * suports windows linux and mac line endings
+     */
+    void safeGetLine( std::string& t )
+    {
+        t.clear( );
+        DEBUG( uiNumLinesRead++; ) // DEBUG
+
+        // The characters in the stream are read one-by-one using a std::streambuf.
+        // That is faster than reading them one-by-one using the std::istream.
+        // Code that uses streambuf this way must be guarded by a sentry object.
+        // The sentry object performs various tasks,
+        // such as thread synchronization and updating the stream state.
+
+        std::istream::sentry se( xStream, true );
+        std::streambuf* sb = xStream.rdbuf( );
+
+        while( true )
+        {
+            int c = sb->sbumpc( );
+            switch( c )
+            {
+                case '\n':
+                    return;
+                case '\r':
+                    if( sb->sgetc( ) == '\n' )
+                        sb->sbumpc( );
+                    return;
+                case std::streambuf::traits_type::eof( ):
+                    // Also handle the case when the last line has no line ending
+                    if( t.empty( ) )
+                        xStream.setstate( std::ios::eofbit );
+                    return;
+                default:
+                    t += (char)c;
+            } // switch
+        } // while
+    } // method
+}; // class
+
+class GzFileStream : public FileStream
+{
+  private:
+    // gzFile is a pointer type
+    gzFile pFile;
+    int lastReadReturn = 1; // 1 == last read was ok; 0 == eof; -1 == error
+    char cBuff;
+
+  public:
+    GzFileStream( std::string sFilename )
+    {
+        // open the file in reading and binary mode
+        pFile = gzopen( sFilename.c_str( ), "rb" );
+        if( pFile != nullptr )
+            // fill internal buffer
+            lastReadReturn = gzread( pFile, &cBuff, 1 );
+    } // constructor
+
+    bool is_open( ) const
+    {
+        return pFile != nullptr;
+    } // method
+
+    bool eof( ) const
+    {
+        return lastReadReturn != 1;
+    } // method
+
+    void close( )
+    {
+        gzclose( pFile );
+    } // method
+
+    size_t tellg( )
+    {
+        return gzoffset( pFile );
+    } // method
+
+    char peek( )
+    {
+        return cBuff;
+    } // method
+
+    void safeGetLine( std::string& t )
+    {
+        t.clear( );
+        DEBUG( uiNumLinesRead++; ) // DEBUG
+
+        while( true )
+        {
+            // gzread returns the number of bytes read; if we do not get 1 here something went wrong
+            if( lastReadReturn != 1 )
+                break;
+            // if we reached the end of the line return
+            if(cBuff == '\r')
+                // read the \n that should be the next character
+                lastReadReturn = gzread( pFile, &cBuff, 1 );
+            if( cBuff == '\n')
+                break;
+            t += cBuff;
+
+            // read one char from the stream
+            lastReadReturn = gzread( pFile, &cBuff, 1 );
+        } // while
+
+        // read one char from the stream
+        lastReadReturn = gzread( pFile, &cBuff, 1 );
+    } // method
+}; // class
+
+
 class Reader
 {
   public:
@@ -31,15 +218,21 @@ class Reader
 class FileReader : public Module<NucSeq, true>, public Reader
 {
   public:
-    std::shared_ptr<std::ifstream> pFile;
+    std::shared_ptr<FileStream> pFile;
     size_t uiFileSize = 0;
     DEBUG( size_t uiNumLinesRead = 0; size_t uiNumLinesWithNs = 0; ) // DEBUG
     std::shared_ptr<NucSeq> EXPORTED execute( );
     /**
      * @brief creates a new FileReader.
      */
-    FileReader( std::string sFileName ) : pFile( new std::ifstream( sFileName ) )
+    FileReader( std::string sFileName )
     {
+#ifdef WITH_ZLIB
+        if( sFileName.size( ) > 3 && sFileName.substr( sFileName.size( ) - 3, 3 ) == ".gz" )
+            pFile = std::make_shared<GzFileStream>( sFileName );
+        else
+#endif
+            pFile = std::make_shared<StdFileStream>( sFileName );
         if( !pFile->is_open( ) )
         {
             throw AnnotatedException( "Unable to open file" + sFileName );
@@ -66,7 +259,7 @@ class FileReader : public Module<NucSeq, true>, public Reader
 
     size_t getCurrPosInFile( ) const
     {
-        if( !pFile->good( ) || pFile->eof( ) )
+        if( pFile->eof( ) )
             return uiFileSize;
         return pFile->tellg( );
     } // function
@@ -78,47 +271,6 @@ class FileReader : public Module<NucSeq, true>, public Reader
             return 1;
         return uiFileSize;
     } // function
-  private:
-    /**
-     * code taken from
-     * https://stackoverflow.com/questions/6089231/getting-std-ifstream-to-handle-lf-cr-and-crlf
-     * suports windows linux and mac line endings
-     */
-    inline void safeGetline( std::string& t )
-    {
-        t.clear( );
-        DEBUG( uiNumLinesRead++; ) // DEBUG
-
-        // The characters in the stream are read one-by-one using a std::streambuf.
-        // That is faster than reading them one-by-one using the std::istream.
-        // Code that uses streambuf this way must be guarded by a sentry object.
-        // The sentry object performs various tasks,
-        // such as thread synchronization and updating the stream state.
-
-        std::istream::sentry se( *pFile, true );
-        std::streambuf* sb = pFile->rdbuf( );
-
-        while( true )
-        {
-            int c = sb->sbumpc( );
-            switch( c )
-            {
-                case '\n':
-                    return;
-                case '\r':
-                    if( sb->sgetc( ) == '\n' )
-                        sb->sbumpc( );
-                    return;
-                case std::streambuf::traits_type::eof( ):
-                    // Also handle the case when the last line has no line ending
-                    if( t.empty( ) )
-                        pFile->setstate( std::ios::eofbit );
-                    return;
-                default:
-                    t += (char)c;
-            } // switch
-        } // while
-    } // method
 }; // class
 
 typedef ContainerVector<std::shared_ptr<NucSeq>> TP_PAIRED_READS;
