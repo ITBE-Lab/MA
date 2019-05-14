f80b96ad6f50fc65554d731731d06167c71d2d6d
Markus <markus.rainer.schmidt@gmail.com>
2019-05-14 21:14:52 +0900

    removed test code

--- libs/ma/src/container/svDb.cpp
+++ libs/ma/src/container/svDb.cpp
@@ -93,7 +93,7 @@ void exportSoCDbWriter( py::module& rxPyModuleId )
         .def( "hasNext", &SvCallsFromDb::hasNext );
 
     // export the NucSeqFromSql classes
-    exportModule<Cyclic<AllNucSeqFromSql, std::shared_ptr<SV_DB>>, std::shared_ptr<SV_DB>>( rxPyModuleId,
+    exportModule<AllNucSeqFromSql, std::shared_ptr<SV_DB>>( rxPyModuleId,
                                                                                             "AllNucSeqFromSql" );
     exportModule<NucSeqFromSql, std::shared_ptr<SV_DB>>( rxPyModuleId, "NucSeqFromSql" );
     exportModule<PairedNucSeqFromSql, std::shared_ptr<SV_DB>>( rxPyModuleId, "PairedNucSeqFromSql" );
