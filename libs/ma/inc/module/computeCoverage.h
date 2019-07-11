dbde824fab31cbab701f0ef4ba04725c0ecff7a9
Markus <markus.rainer.schmidt@gmail.com>
2019-07-11 16:35:10 +0900

    changing coverage from median to average

--- libs/ma/inc/module/computeCoverage.h
+++ libs/ma/inc/module/computeCoverage.h
@@ -22,11 +22,21 @@ namespace libMA
  */
 class ComputeCoverage : public Module<Container, false, SuffixArrayInterface, NucSeq, SegmentVector>
 {
+
+  public:
+    std::shared_ptr<SV_DB> pDb;
+    const unsigned int uiMaxAmbiguity;
+    ///
+    const size_t uiMinSeedSize;
+    const static size_t uiMaxDistance = 50;
+    const size_t uiAllowedOverlap = 3;
+
     class SvCallWrapper
     {
       public:
         SvCall xCall;
-        std::array<std::vector<Seed>, 2> avCoverageAnalysis;
+        // 0: left-from, 1:left-to, 2: right-from, 3:right-to
+        std::array<std::vector<Seed>, 4> avCoverageAnalysis;
         std::mutex xMutex;
 
         SvCallWrapper( SvCall xCall ) : xCall( xCall ), avCoverageAnalysis( ), xMutex( )
@@ -44,11 +54,10 @@ class ComputeCoverage : public Module<Container, false, SuffixArrayInterface, Nu
             bool bIsLeft = rS.start_ref( ) + uiAllowedOverlap < uiStart;
             bool bIsRight = rS.end_ref( ) > uiEnd + uiAllowedOverlap;
 
-
             if( bIsLeft )
-                avCoverageAnalysis[ xCall.bSwitchStrand && !bIsFrom ? 1 : 0 ].push_back( rS );
+                avCoverageAnalysis[ bIsFrom ? 0 : 1 ].push_back( rS );
             if( bIsRight )
-                avCoverageAnalysis[ xCall.bSwitchStrand && !bIsFrom ? 0 : 1 ].push_back( rS );
+                avCoverageAnalysis[ bIsFrom ? 2 : 3 ].push_back( rS );
 
             return true;
         } // method
@@ -56,7 +65,7 @@ class ComputeCoverage : public Module<Container, false, SuffixArrayInterface, Nu
         inline void addSeed( Seed& rS, std::shared_ptr<NucSeq> pQuery, const size_t uiMaxDistance,
                              const size_t uiAllowedOverlap )
         {
-            std::lock_guard<std::mutex> xGuard( xMutex ); 
+            std::lock_guard<std::mutex> xGuard( xMutex );
             size_t uiNumFound = 0;
 
             if( this->checkInterval( this->xCall.uiFromStart, this->xCall.uiFromStart + this->xCall.uiFromSize, rS,
@@ -70,71 +79,130 @@ class ComputeCoverage : public Module<Container, false, SuffixArrayInterface, Nu
             assert( uiNumFound > 0 );
         } // method
 
+        inline nucSeqIndex dist_in_area( nucSeqIndex uiFrom, nucSeqIndex uiTo, size_t uiIdx )
+        {
+            assert( uiTo >= uiFrom );
+            switch( uiIdx )
+            {
+                case 0:
+                    if( uiFrom + ComputeCoverage::uiMaxDistance < xCall.uiFromStart )
+                        uiFrom = ComputeCoverage::uiMaxDistance < xCall.uiFromStart
+                                     ? xCall.uiFromStart - ComputeCoverage::uiMaxDistance
+                                     : 0;
+                    if( uiTo > xCall.uiFromStart + xCall.uiFromSize )
+                        uiTo = xCall.uiFromStart + xCall.uiFromSize;
+                    break;
+                case 1:
+                    if( uiFrom + ComputeCoverage::uiMaxDistance < xCall.uiToStart )
+                        uiFrom = ComputeCoverage::uiMaxDistance < xCall.uiToStart
+                                     ? xCall.uiToStart - ComputeCoverage::uiMaxDistance
+                                     : 0;
+                    if( uiTo > xCall.uiToStart + xCall.uiToSize )
+                        uiTo = xCall.uiToStart + xCall.uiToSize;
+                    break;
+                case 2:
+                    if( uiTo > xCall.uiFromStart + xCall.uiFromSize + ComputeCoverage::uiMaxDistance )
+                        uiTo = xCall.uiFromStart + xCall.uiFromSize + ComputeCoverage::uiMaxDistance;
+                    if( uiFrom < xCall.uiFromStart )
+                        uiFrom = xCall.uiFromStart;
+                    break;
+                case 3:
+                    if( uiTo > xCall.uiToStart + xCall.uiToSize + ComputeCoverage::uiMaxDistance )
+                        uiTo = xCall.uiToStart + xCall.uiToSize + ComputeCoverage::uiMaxDistance;
+                    if( uiFrom < xCall.uiToStart )
+                        uiFrom = xCall.uiToStart;
+                    break;
+                default:
+                    assert( false );
+                    break;
+            } // switch
+            if( uiTo >= uiFrom )
+                return 0;
+
+            return uiTo - uiFrom;
+        } // method
+
+        inline void fill_coverage_list( std::vector<std::pair<size_t, nucSeqIndex>>& vCoverageList,
+                                        size_t& uiCoverageSum, size_t uiIdx )
+        {
+            if( avCoverageAnalysis[ uiIdx ].size( ) == 0 )
+                return;
+            // sort seeds by reference position
+            std::sort( avCoverageAnalysis[ uiIdx ].begin( ), avCoverageAnalysis[ uiIdx ].end( ),
+                       []( Seed& rA, Seed& rB ) { return rA.start_ref( ) < rB.start_ref( ); } );
+
+
+            std::vector<Seed>::iterator itStart = avCoverageAnalysis[ uiIdx ].begin( );
+            std::vector<Seed>::iterator itEnd = avCoverageAnalysis[ uiIdx ].begin( );
+            size_t uiCoverageCnt = 0;
+            nucSeqIndex uiLastPos = itStart->start_ref( );
+
+            while( itStart != avCoverageAnalysis[ uiIdx ].end( ) || itEnd != avCoverageAnalysis[ uiIdx ].end( ) )
+            {
+                if( itStart != avCoverageAnalysis[ uiIdx ].end( ) && itStart->start_ref( ) < itEnd->end_ref( ) )
+                {
+                    if( itStart->start_ref( ) > uiLastPos )
+                    {
+                        nucSeqIndex uiLength = dist_in_area( uiLastPos, itStart->start_ref( ), uiIdx );
+                        if(uiLength > 0)
+                        {
+                            vCoverageList.emplace_back( uiCoverageCnt, uiLength );
+                            uiCoverageSum += uiLength;
+                        } // if
+                    } // if
+                    uiCoverageCnt++;
+                    uiLastPos = itStart->start_ref( );
+                    itStart++;
+                } // if
+                else
+                {
+                    if( itEnd->end_ref( ) > uiLastPos )
+                    {
+                        nucSeqIndex uiLength = dist_in_area( uiLastPos, itEnd->end_ref( ), uiIdx );
+                        if(uiLength > 0)
+                        {
+                            vCoverageList.emplace_back( uiCoverageCnt, uiLength );
+                            uiCoverageSum += uiLength;
+                        } // if
+                    } // if
+                    uiCoverageCnt--;
+                    uiLastPos = itEnd->end_ref( );
+
+                    itEnd++;
+                } // else
+            } // while
+        } // method
+
         inline void compute_coverage( )
         {
             xCall.uiCoverage = 1;
-            for( size_t uiIdx : {0, 1} )
-                if( avCoverageAnalysis[ uiIdx ].size( ) > 0 )
-                {
-                    // sort seeds by reference position
-                    std::sort( avCoverageAnalysis[ uiIdx ].begin( ), avCoverageAnalysis[ uiIdx ].end( ),
-                               []( Seed& rA, Seed& rB ) { return rA.start_ref( ) < rB.start_ref( ); } );
 
-                    std::vector<std::pair<size_t, nucSeqIndex>> vCoverageList;
+            for( size_t uiIdx : {0, 2} )
+            {
+                std::vector<std::pair<size_t, nucSeqIndex>> vCoverageList;
+                size_t uiCoverageSum = 0;
 
-                    std::vector<Seed>::iterator itStart = avCoverageAnalysis[ uiIdx ].begin( );
-                    std::vector<Seed>::iterator itEnd = avCoverageAnalysis[ uiIdx ].begin( );
-                    size_t uiCoverageCnt = 0;
-                    size_t uiCoverageSum = 0;
-                    nucSeqIndex uiLastPos = itStart->start_ref( );
+                fill_coverage_list( vCoverageList, uiCoverageSum, uiIdx );
+                // depending on bSwitch strand different sides go together...
+                // if we don't switch strands we have to combine one 'left' with one 'right'
+                // if we switch strand we have to match the two 'left's and two 'right's
+                // we always have to pick one 'from' and one 'to' together.
+                fill_coverage_list( vCoverageList, uiCoverageSum, xCall.bSwitchStrand ? uiIdx + 1 : 3 - uiIdx );
 
-                    while( itStart != avCoverageAnalysis[ uiIdx ].end( ) ||
-                           itEnd != avCoverageAnalysis[ uiIdx ].end( ) )
-                    {
-                        if( itStart != avCoverageAnalysis[ uiIdx ].end( ) && itStart->start_ref( ) < itEnd->end_ref( ) )
-                        {
-                            if( itStart->start_ref( ) > uiLastPos )
-                            {
-                                vCoverageList.emplace_back( uiCoverageCnt, uiLastPos - itStart->start_ref( ) );
-                                uiCoverageSum += uiLastPos - itStart->start_ref( );
-                            } // if
-                            uiCoverageCnt++;
-                            uiLastPos = itStart->start_ref( );
-                            itStart++;
-                        } // if
-                        else
-                        {
-                            if( itEnd->end_ref( ) > uiLastPos )
-                            {
-                                vCoverageList.emplace_back( uiCoverageCnt, uiLastPos - itEnd->end_ref( ) );
-                                uiCoverageSum += uiLastPos - itEnd->end_ref( );
-                            } // if
-                            uiCoverageCnt--;
-                            uiLastPos = itEnd->end_ref( );
+                if( uiCoverageSum == 0 )
+                    continue;
 
-                            itEnd++;
-                        } // else
-                    } // while
+                std::sort( vCoverageList.begin( ), vCoverageList.end( ) );
+                size_t uiIdx2 = 0;
+                while( uiIdx2 + 1 < vCoverageList.size( ) && uiCoverageSum > vCoverageList[ uiIdx2 ].second * 2 )
+                    uiCoverageSum -= vCoverageList[ uiIdx2++ ].second * 2;
 
-                    std::sort( vCoverageList.begin( ), vCoverageList.end( ) );
-                    size_t uiIdx2 = 0;
-                    while( uiIdx2 + 1 < vCoverageList.size( ) && uiCoverageSum > vCoverageList[ uiIdx2 ].second * 2 )
-                        uiCoverageSum -= vCoverageList[ uiIdx2++ ].second * 2;
-
-                    assert( vCoverageList[ uiIdx2 ].first < 1000 );
-                    if( vCoverageList[ uiIdx2 ].first > xCall.uiCoverage )
-                        xCall.uiCoverage = (uint32_t)vCoverageList[ uiIdx2 ].first;
-                } // if
+                assert( vCoverageList[ uiIdx2 ].first < 1000 );
+                if( vCoverageList[ uiIdx2 ].first > xCall.uiCoverage )
+                    xCall.uiCoverage = (uint32_t)vCoverageList[ uiIdx2 ].first;
+            } // for
         } // method
     }; // class
-
-  public:
-    std::shared_ptr<SV_DB> pDb;
-    const unsigned int uiMaxAmbiguity;
-    ///
-    const size_t uiMinSeedSize;
-    const size_t uiMaxDistance = 50;
-    const size_t uiAllowedOverlap = 3;
 
     std::shared_ptr<interval_tree::IntervalTree<nucSeqIndex, std::shared_ptr<SvCallWrapper>>> pIntervalTree;
     std::vector<std::shared_ptr<SvCallWrapper>> vCalls;
